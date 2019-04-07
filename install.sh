7acaecc2392b6d4110e478d4ec3400e6d730b41c
lipan <562150688@qq.com>
2019-04-07 19:35:40 +0800

    增加辅助安装cli和logrus包的脚本.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 辅助安装cli和logrus包
+
+# to: 检查环境变量GOROOT,GOPATH是否存在
+
+WORKDIR=${GOPATH}/src
+
+cd ${WORKDIR}
+
+# todo: 失败退出
+go get github.com/urfave/cli
+echo "cli install success."
+
+# todo: 失败退出
+mkdir -p golang.org/x && cd golang.org/x                                  
+git clone https://github.com/golang/sys.git
+cd sys/unix
+go install
+go get github.com/Sirupsen/logrus
+echo "logrus install success."
